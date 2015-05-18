6a3f3200e2e7175c0cdbb0d4667bc85191c0b501
ndaci <daci.nadir@gmail.com>
2015-05-18 10:10:59 +0200

    Add zoomed versions of the ROC curves. Add protections against inexistant graphs pointers.

--- MonoXAnalysis/AnalysisStep/macros/rocCurve.C
+++ MonoXAnalysis/AnalysisStep/macros/rocCurve.C
@@ -18,6 +18,10 @@
 //
 #include "tdrstyle.h"
 
+Int_t setStyle(TGraph* g, Int_t marker, Int_t color, 
+	       UInt_t nB, TString title, Bool_t zoom);
+Int_t setStyle(TLegend* leg);
+
 Int_t rocCurve(TString _tag="v11_XMA_QCD_ROC_FinerBinning", bool dolog=false)
 {
 
@@ -36,11 +40,13 @@ Int_t rocCurve(TString _tag="v11_XMA_QCD_ROC_FinerBinning", bool dolog=false)
 
   // Selections and variables
   const UInt_t nWd=2; // forward/backward cumulative distribution
-  const UInt_t nS=5;
-  const UInt_t nV=5;
+  const UInt_t nS=5;  // selections
+  const UInt_t nV=5;  // variables
+  const UInt_t nGZ=2;  // full/zoomed
 
-  TGraph* gRoc[nS][nV][nWd];
+  TGraph* gRoc[nS][nV][nWd][nGZ];
 
+  TString tzoom[nGZ] = {"_full", "_zoom"}; 
   TString wd[nWd]    = {"upcut","lowcut"};
   TString wdT[nWd]   = {"(upper cut)","(lower cut)"};
   TString select[nS] = {"alljets","monojet","1jet","2jet","3jet"};
@@ -50,7 +56,7 @@ Int_t rocCurve(TString _tag="v11_XMA_QCD_ROC_FinerBinning", bool dolog=false)
 
   //UInt_t  nBins[nV]  = {40, 50, 50, 50,  50};
   //UInt_t  nBins[nV]  = {400, 500, 500, 500, 500};
-  UInt_t  nBins[nV]  = {800, 1000, 1000, 1000,  1000};
+  //UInt_t  nBins[nV]  = {800, 1000, 1000, 1000,  1000};
   //UInt_t  nBins[nV]  = {4000, 5000, 5000, 5000,  5000};
   //UInt_t  nBins[nV]  = {8000, 10000, 10000, 10000,  10000};
   
@@ -60,9 +66,11 @@ Int_t rocCurve(TString _tag="v11_XMA_QCD_ROC_FinerBinning", bool dolog=false)
   for(UInt_t iS=0 ; iS<nS ; iS++) {
     for(UInt_t iV=0 ; iV<nV ; iV++) {
 
+      // Get input QCD and Znn distributions from the file
       TH1F* h_qcd = (TH1F*) file->Get("h_"+var[iV]+"_qcd_"+select[iS]);
       TH1F* h_znn = (TH1F*) file->Get("h_"+var[iV]+"_znn_"+select[iS]);
 
+      // Check that the histograms were found
       if(!h_qcd) {
 	cout << "ERROR: missing histogram" << endl
 	     << "h_"+var[iV]+"_qcd_"+select[iS] << endl;
@@ -73,24 +81,27 @@ Int_t rocCurve(TString _tag="v11_XMA_QCD_ROC_FinerBinning", bool dolog=false)
       }
       if(!h_qcd || !h_znn) return -1;
 
+      // Print the integral of the histograms
       cout << "-- "+var[iV]+" "+select[iS]+" : Integrals : "
 	   << "qcd=" << h_qcd->Integral() << " "
 	   << "znn=" << h_znn->Integral() << " " << endl;
 
-      const UInt_t nB = nBins[iV];
+      // Get the histograms binning
+      //const UInt_t nB = nBins[iV];
+      const UInt_t nB = h_qcd->GetNbinsX();
 
+      // Build the graph from cumulative distributions
+      // for each backward/forward case
       for(UInt_t iWd=0 ; iWd<nWd ; iWd++) {
 	Double_t cum_qcd=0, cum_znn=0;
 	Double_t yCum_qcd[nB];
 	Double_t yCum_znn[nB];
 
+	// Prepare full version of the graph
+	/// Loop over the bins of the input distributions
 	for(UInt_t iB=0 ; iB<nB ; iB++) {
-
 	  cum_qcd += h_qcd->GetBinContent(iB+1);
 	  cum_znn += h_znn->GetBinContent(iB+1);
-	  //cum_qcd += h_qcd->GetBinContent(nB-iB);
-	  //cum_znn += h_znn->GetBinContent(nB-iB);
-
 	  if(iWd==0) { // upper cut : x < cut
 	    yCum_qcd[iB] = cum_qcd;
 	    yCum_znn[iB] = cum_znn;
@@ -99,78 +110,144 @@ Int_t rocCurve(TString _tag="v11_XMA_QCD_ROC_FinerBinning", bool dolog=false)
 	    yCum_qcd[iB] = 1 - cum_qcd;
 	    yCum_znn[iB] = 1 - cum_znn;
 	  }
-	  
 	}
-
-	gRoc[iS][iV][iWd] = new TGraph(nB, yCum_znn, yCum_qcd);
-	gRoc[iS][iV][iWd]->SetMarkerStyle(kOpenSquare);
-	gRoc[iS][iV][iWd]->SetMarkerColor(colors[iV]);
-	gRoc[iS][iV][iWd]->SetLineColor(colors[iV]);
-	//gRoc[iS][iV][iWd]->SetMarkerSize();
-	gRoc[iS][iV][iWd]->SetFillColor(kWhite);
-	gRoc[iS][iV][iWd]->GetXaxis()->Set(nB, 0, 1);
-	gRoc[iS][iV][iWd]->GetYaxis()->Set(nB, 0, 1);
-	gRoc[iS][iV][iWd]->SetMinimum(0);
-	gRoc[iS][iV][iWd]->SetMaximum(1.05);
-	gRoc[iS][iV][iWd]->GetXaxis()->SetTitle("Z(#nu#nu) efficiency");
-	gRoc[iS][iV][iWd]->GetYaxis()->SetTitle("QCD efficiency");
-	gRoc[iS][iV][iWd]->SetTitle("ROC Curve : "+select[iS]+" "+var[iV]+" "+wdT[iWd]);
-	gRoc[iS][iV][iWd]->Draw("AL");
-
-	//cRoc.Print("plots/"+_tag+"/roc_"+select[iS]+"_"+var[iV]+"_"+wd[iWd]+".pdf","pdf");
-
+	///
+	/// use the arrays to fill the graph and draw it
+	gRoc[iS][iV][iWd][0] = new TGraph(nB, yCum_znn, yCum_qcd);
+	setStyle(gRoc[iS][iV][iWd][0], kOpenSquare, colors[iV], nB,
+		 "ROC Curve : "+select[iS]+" "+var[iV]+" "+wdT[iWd], 
+		 kFALSE);
+	if(gRoc[iS][iV][iWd][0]) gRoc[iS][iV][iWd][0]->Draw("AL");	
+	///
+	/// print the graph in a single pdf file
+	TString pdftitle = "Title:"+select[iS]+" "+var[iV]+" "+wd[iWd];
 	if(iS==0 && iV==0 && iWd==0) 
-	  cRoc.Print("plots/"+_tag+"/roc.pdf(","Title:"+select[iS]+" "+var[iV]+" "+wd[iWd]);
+	  cRoc.Print("plots/"+_tag+"/rocfull.pdf(",pdftitle);
 	else if(iS==nS-1 && iV==nV-1 && iWd==nWd-1)
-	  cRoc.Print("plots/"+_tag+"/roc.pdf)","Title:"+select[iS]+" "+var[iV]+" "+wd[iWd]);
+	  cRoc.Print("plots/"+_tag+"/rocfull.pdf)",pdftitle);
 	else
-	  cRoc.Print("plots/"+_tag+"/roc.pdf","Title:"+select[iS]+" "+var[iV]+" "+wd[iWd]);
+	  cRoc.Print("plots/"+_tag+"/rocfull.pdf",pdftitle);
+	// end full version
+
+	// Prepare zoomed version by extracting interesting region
+	/// determine indices of points where eff(znn)>60%
+	vector<UInt_t> idxZoom;
+	idxZoom.clear();
+	for(UInt_t iB=0 ; iB<nB ; iB++) {
+	  if(yCum_znn[iB]>0.60) idxZoom.push_back(iB);
+	}
+	///
+	/// use the indices to build arrays
+	const UInt_t nZ = idxZoom.size();
+	Double_t yZoo_qcd[nZ];
+	Double_t yZoo_znn[nZ];
+	for(UInt_t iZ=0 ; iZ<nZ ; iZ++) {	
+	  yZoo_qcd[iZ] = yCum_qcd[idxZoom[iZ]];
+	  yZoo_znn[iZ] = yCum_znn[idxZoom[iZ]];
+	}
+	///
+	/// use the arrays to fill the graph and draw it
+	gRoc[iS][iV][iWd][1] = new TGraph(nZ, yZoo_znn, yZoo_qcd);
+	setStyle(gRoc[iS][iV][iWd][1], kOpenSquare, colors[iV], nZ,
+		 "ROC Curve : "+select[iS]+" "+var[iV]+" "+wdT[iWd],
+		 kTRUE);
+	if(gRoc[iS][iV][iWd][1]) gRoc[iS][iV][iWd][1]->Draw("AL");	
+	///
+	/// print the graph in a single pdf file
+	if(iS==0 && iV==0 && iWd==0) 
+	  cRoc.Print("plots/"+_tag+"/roczoom.pdf(",pdftitle);
+	else if(iS==nS-1 && iV==nV-1 && iWd==nWd-1)
+	  cRoc.Print("plots/"+_tag+"/roczoom.pdf)",pdftitle);
+	else
+	  cRoc.Print("plots/"+_tag+"/roczoom.pdf",pdftitle);
+	// end zoomed version
 
       } // end loop over nWd
     }   // end loop over nV
   }     // end loop over nS
 
   // Put several killers per plot
-  for(UInt_t iS=0 ; iS<nS ; iS++) {
+  for(UInt_t iGZ=0 ; iGZ<nGZ ; iGZ++) {
+      
+    for(UInt_t iS=0 ; iS<nS ; iS++) {
 
-    gRoc[iS][0][1]->SetTitle("Selection: "+select[iS]);
+      gRoc[iS][0][1][iGZ]->SetTitle("Selection: "+select[iS]);
 
-    gRoc[iS][0][1]->Draw("AL"); // alphat
-    gRoc[iS][1][1]->Draw("L");  // apcjetmetmax
-    gRoc[iS][2][1]->Draw("L");  // apcjetmetmin
-    gRoc[iS][3][0]->Draw("L");  // jetjetdphi
-    gRoc[iS][4][1]->Draw("L");  // jetmetdphimin
+      if(gRoc[iS][0][1][iGZ]->GetN()>0) gRoc[iS][0][1][iGZ]->Draw("AL"); // alphat
+      else if(gRoc[iS][1][1][iGZ]->GetN()>0) gRoc[iS][1][1][iGZ]->Draw("AL");  // apcjetmetmax
+      else if(gRoc[iS][2][1][iGZ]->GetN()>0) gRoc[iS][2][1][iGZ]->Draw("AL");  // apcjetmetmin
+      else if(gRoc[iS][3][0][iGZ]->GetN()>0) gRoc[iS][3][0][iGZ]->Draw("AL");  // jetjetdphi
+      else if(gRoc[iS][4][1][iGZ]->GetN()>0) gRoc[iS][4][1][iGZ]->Draw("AL");  // jetmetdphimin
 
-    TLegend* leg = new TLegend(0.38,0.6,0.58,0.8,"","brNDC");
-    leg->SetLineColor(1);
-    leg->SetTextColor(1);
-    leg->SetTextFont(42);
-    leg->SetTextSize(0.0244755);
-    leg->SetShadowColor(kWhite);
-    leg->SetFillColor(kWhite);
+      if(gRoc[iS][0][1][iGZ]->GetN()>0) gRoc[iS][0][1][iGZ]->Draw("L"); // alphat
+      if(gRoc[iS][1][1][iGZ]->GetN()>0) gRoc[iS][1][1][iGZ]->Draw("L");  // apcjetmetmax
+      if(gRoc[iS][2][1][iGZ]->GetN()>0) gRoc[iS][2][1][iGZ]->Draw("L");  // apcjetmetmin
+      if(gRoc[iS][3][0][iGZ]->GetN()>0) gRoc[iS][3][0][iGZ]->Draw("L");  // jetjetdphi
+      if(gRoc[iS][4][1][iGZ]->GetN()>0) gRoc[iS][4][1][iGZ]->Draw("L");  // jetmetdphimin
 
-    for(UInt_t iV=0 ; iV<nV ; iV++)
-      leg->AddEntry(gRoc[iS][iV][1], var[iV], "L");
+      TLegend* leg = new TLegend(0.38,0.6,0.58,0.8,"","brNDC");
+      setStyle(leg);
+    
+      for(UInt_t iV=0 ; iV<nV ; iV++)
+	leg->AddEntry(gRoc[iS][iV][1][iGZ], var[iV], "L");
 
-    leg->Draw();
+      leg->Draw();
 
-    TString metCut;
-    if(_tag.Contains("NoMetCut"))    metCut = "NoMetCut";
-    else if(_tag.Contains("Met200")) metCut = "Met200";
-    else if(_tag.Contains("Met350")) metCut = "Met350";
-    else if(_tag.Contains("MetFrom0to200"))   metCut = "MetFrom0to200";
-    else if(_tag.Contains("MetFrom200to250")) metCut = "MetFrom200to250";
-    else if(_tag.Contains("MetFrom250to350")) metCut = "MetFrom250to350";
-    TString mysel = select[iS]+"_"+metCut;
+      TString metCut;
+      if(_tag.Contains("NoMetCut"))    metCut = "NoMetCut";
+      else if(_tag.Contains("Met200")) metCut = "Met200";
+      else if(_tag.Contains("Met350")) metCut = "Met350";
+      else if(_tag.Contains("MetFrom0to200"))   metCut = "MetFrom0to200";
+      else if(_tag.Contains("MetFrom200to250")) metCut = "MetFrom200to250";
+      else if(_tag.Contains("MetFrom250to350")) metCut = "MetFrom250to350";
+      TString mysel = select[iS]+"_"+metCut;
 
-    if(iS==0) 
-      cRoc.Print("plots/"+_tag+"/allroc.pdf(","Title:"+mysel);
-    else if(iS==nS-1)
-      cRoc.Print("plots/"+_tag+"/allroc.pdf)","Title:"+mysel);
-    else
-      cRoc.Print("plots/"+_tag+"/allroc.pdf","Title:"+mysel);
+      if(iS==0) 
+	cRoc.Print("plots/"+_tag+"/allroc"+tzoom[iGZ]+".pdf(","Title:"+mysel);
+      else if(iS==nS-1)
+	cRoc.Print("plots/"+_tag+"/allroc"+tzoom[iGZ]+".pdf)","Title:"+mysel);
+      else
+	cRoc.Print("plots/"+_tag+"/allroc"+tzoom[iGZ]+".pdf","Title:"+mysel);
 
+    }
   }
+  
+  return 0;
+}
 
+Int_t setStyle(TGraph* g, Int_t marker, Int_t color, 
+	       UInt_t nB, TString title, Bool_t zoom)
+{
+
+  g->SetMarkerStyle(marker);
+  g->SetMarkerColor(color);
+  g->SetLineColor(color);
+  //g->SetMarkerSize();
+  g->SetFillColor(kWhite);
+
+  g->GetXaxis()->Set(nB, 0, 1);
+  g->GetYaxis()->Set(nB, 0, 1);
+  if(zoom) g->GetXaxis()->Set(nB, 0.6, 1);
+
+  g->SetMinimum(0);
+  g->SetMaximum(1.05);
+
+  g->GetXaxis()->SetTitle("Z(#nu#nu) efficiency");
+  g->GetYaxis()->SetTitle("QCD efficiency");
+  g->SetTitle(title);
+
+  return 0;
+}
+
+Int_t setStyle(TLegend* leg)
+{
+
+  leg->SetLineColor(1);
+  leg->SetTextColor(1);
+  leg->SetTextFont(42);
+  leg->SetTextSize(0.0244755);
+  leg->SetShadowColor(kWhite);
+  leg->SetFillColor(kWhite);
+  
   return 0;
 }
