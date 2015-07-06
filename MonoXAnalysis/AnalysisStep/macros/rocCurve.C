7460efb6b05c5b701051206c8626e72427d8a2d4
ndaci <daci.nadir@gmail.com>
2015-07-06 09:43:30 +0200

    Set Met cut as a scanCut instead of input directory suffix. Loop over Met scanCut to get distributions. Indent because extra loop over iCut.

--- MonoXAnalysis/AnalysisStep/macros/rocCurve.C
+++ MonoXAnalysis/AnalysisStep/macros/rocCurve.C
@@ -86,8 +86,13 @@ Int_t rocCurve(TString _tag="",
 
   Int_t colors[nV]   = {kBlack, kBlue, kRed, kGreen+2, kMagenta};
 
+  /*
   const UInt_t nCut=1;
   TString scanCut[  nCut] = {"1"};
+  */
+
+  const UInt_t nCut=4;
+  TString scanCut[  nCut] = {"Met200", "MetFrom200to250", "MetFrom250to350", "Met350"};
 
   Int_t  wpQCD_bin,wpZNN_bin;
   Float_t wpQCD_cut,wpZNN_cut;
@@ -100,254 +105,249 @@ Int_t rocCurve(TString _tag="",
   effQCD_wpQCD = effZNN_wpQCD = effQCD_wpZNN = effZNN_wpZNN = -1;
 
   cout << "- Ready to loop over selections and variables" << endl;
+
+  for(UInt_t iC=0 ; iC<nCut ; iC++) {
   
-  for(UInt_t iS=0 ; iS<nS ; iS++) {
-    
-    cout << "-- selection: " << select[iS] << endl;
+    cout << "-- MET bin: " << scanCut[iC] << endl;
 
-    for(UInt_t iV=0 ; iV<nV ; iV++) {
-
-      cout << "--- variable: " << var[iV] << endl;
-
-      // Get input QCD and Znn distributions from the file
-      TString nameQCD = "h_"+var[iV]+"_qcd_"+select[iS]+"_"+scanCut[0];
-      cout << "--- getting: " << nameQCD ;
-      TH1F* h_qcd = (TH1F*) file->Get(nameQCD);
-      cout << "... done!" << endl;
-
-      TString nameZnn = "h_"+var[iV]+"_znn_"+select[iS]+"_"+scanCut[0];
-      cout << "--- getting: " << nameQCD ;
-      TH1F* h_znn = (TH1F*) file->Get(nameZnn);
-      cout << "... done!" << endl;
-
-      // B/S histogram
-      //hBoS[iS][iV] = (TH1F*)h_znn->Clone("hBoS_"+var[iV]+"_"+select[iS]);
-      //hBoS[iS][iV]->Reset();
-
-      // Check that the histograms were found
-      if(!h_qcd) {
-	cout << "ERROR: missing histogram" << endl
-	     << "h_"+var[iV]+"_qcd_"+select[iS] << endl;
-      }
-      if(!h_znn) {
-	cout << "ERROR: missing histogram" << endl
-	     << "h_"+var[iV]+"_znn_"+select[iS] << endl;
-      }
-      if(!h_qcd || !h_znn) return -1;
-
-      // Print the integral of the histograms
-      cout << "--- going to print the fucking integral." << endl;
-      yieldQCD = h_qcd->Integral();
-      yieldZNN = h_znn->Integral();
-      if(unity && yieldQCD!=0) h_qcd->Scale(1/yieldQCD);
-      if(unity && yieldZNN!=0) h_znn->Scale(1/yieldZNN);
-      cout << "-- "+var[iV]+" "+select[iS]+" : Integrals : "
-	   << "qcd=" << yieldQCD << " "
-	   << "znn=" << yieldZNN << " " << endl;
-
-      // Get the histograms binning
-      //const UInt_t nB = nBins[iV];
-      const UInt_t nB = h_qcd->GetNbinsX();
-
-      // Build the graph from cumulative distributions
-      // for each backward/forward case
-      for(UInt_t iWd=0 ; iWd<nWd ; iWd++) {
-	Float_t cum_qcd=0, cum_znn=0;
-	Float_t yCum_qcd[nB];
-	Float_t yCum_znn[nB];
-
-	// Prepare full version of the graph
-	/// Loop over the bins of the input distributions
-	for(UInt_t iB=0 ; iB<nB ; iB++) {
-	  cum_qcd += h_qcd->GetBinContent(iB+1);
-	  cum_znn += h_znn->GetBinContent(iB+1);
-	  if(iWd==0) { // upper cut : x<cut
-	    yCum_qcd[iB] = cum_qcd;
-	    yCum_znn[iB] = cum_znn;
-	  }
-	  else if(iWd==1) { // lower cut : x>cut
-	    yCum_qcd[iB] = 1 - cum_qcd;
-	    yCum_znn[iB] = 1 - cum_znn;
-	  }
-
-	  // B/S profile
-	  //error  = 0;
-	  //theBos = calcBoS(yieldZNN, yieldQCD, yCum_znn[iB], yCum_qcd[iB], error);
-	  //hBoS[iS][iV]->SetBinContent(iB, theBos);
-	  //hBoS[iS][iV]->SetBinError(  iB, error);
-
-	  // Find bin index corresponding to requested QCD/ZNN efficiency (wpQCD/wpZNN)
-	  if(iB>0) {
-	    if(     iWd==0) { // upper cut x<cut
-	      if(yCum_qcd[iB-1]<wpQCD && yCum_qcd[iB]>=wpQCD) wpQCD_bin = iB;
-	      if(yCum_znn[iB-1]<wpZNN && yCum_znn[iB]>=wpZNN) wpZNN_bin = iB;
-	    }
-	    else if(iWd==1) { // lower cut x>cut
-	      if(yCum_qcd[iB-1]>=wpQCD && yCum_qcd[iB]<wpQCD) wpQCD_bin = iB;
-	      if(yCum_znn[iB-1]>=wpZNN && yCum_znn[iB]<wpZNN) wpZNN_bin = iB;
-	    }
-	  }
-	}
-
-	///////////////////////////////////////////////////////////////////////////////
-	// WORKING POINTS /////////////////////////////////////////////////////////////
-	///////////////////////////////////////////////////////////////////////////////
-	// Find cut value using bin index for requested QCD/ZNN eff
-	wpQCD_cut = h_qcd->GetXaxis()->GetBinCenter(wpQCD_bin);
-	wpZNN_cut = h_znn->GetXaxis()->GetBinCenter(wpZNN_bin);
-	//
-	// Determine corresponding efficiencies
-	// => precise value, for both QCD and ZNN, for each requested efficiency
-	effQCD_wpQCD = (wpQCD_bin>=0 && wpQCD_bin<nB) ? yCum_qcd[wpQCD_bin] : -888888;
-	effZNN_wpQCD = (wpQCD_bin>=0 && wpQCD_bin<nB) ? yCum_znn[wpQCD_bin] : -888888;
-	effQCD_wpZNN = (wpZNN_bin>=0 && wpZNN_bin<nB) ? yCum_qcd[wpZNN_bin] : -888888;
-	effZNN_wpZNN = (wpZNN_bin>=0 && wpZNN_bin<nB) ? yCum_znn[wpZNN_bin] : -888888;
-	//
-	// Write out values
-	outlog << setw(10) << select[iS]
-	       << setw(14) << var[iV]
-	       << setw(12) << wdT[iWd]
-	       << setw(14) << yieldQCD
-	       << setw(14) << yieldZNN
-	       << setw(7 ) << wpQCD
-	       << setw(14) << wpQCD_cut
-	       << setw(14) << effQCD_wpQCD
-	       << setw(14) << effZNN_wpQCD
-	       << setw(14) << yieldQCD*effQCD_wpQCD
-	       << setw(14) << yieldZNN*effZNN_wpQCD
-	       << setw(7 ) << wpZNN
-	       << setw(14) << wpZNN_cut
-	       << setw(14) << effQCD_wpZNN
-	       << setw(14) << effZNN_wpZNN
-	       << setw(14) << yieldQCD*effQCD_wpZNN
-	       << setw(14) << yieldZNN*effZNN_wpZNN
-	       << endl;
-	///////////////////////////////////////////////////////////////////////////////
-
-	///////////////////////////////////////////////////////////////////////////////
-	// GRAPHS /////////////////////////////////////////////////////////////////////
-	///////////////////////////////////////////////////////////////////////////////
-
-	/// use the arrays to fill the graph and draw it
-	gRoc[iS][iV][iWd][0] = new TGraph(nB, yCum_znn, yCum_qcd);
-	setStyle(gRoc[iS][iV][iWd][0], kOpenSquare, colors[iV], nB,
-		 "ROC Curve : "+select[iS]+" "+var[iV]+" "+wdT[iWd], 
-		 kFALSE);
-	if(gRoc[iS][iV][iWd][0]) gRoc[iS][iV][iWd][0]->Draw("AL");	
-	///
-	/// print the graph in a single pdf file
-	TString pdftitle = "Title:"+select[iS]+" "+var[iV]+" "+wd[iWd];
-	if(iS==0 && iV==0 && iWd==0) 
-	  cRoc.Print("plots/"+_tag+"/rocfull.pdf(",pdftitle);
-	else if(iS==nS-1 && iV==nV-1 && iWd==nWd-1)
-	  cRoc.Print("plots/"+_tag+"/rocfull.pdf)",pdftitle);
-	else
-	  cRoc.Print("plots/"+_tag+"/rocfull.pdf",pdftitle);
-	// end full version
-
-	// Prepare zoomed version by extracting interesting region
-	/// determine indices of points where eff(znn)>60%
-	vector<UInt_t> idxZoom;
-	idxZoom.clear();
-	for(UInt_t iB=0 ; iB<nB ; iB++) {
-	  if(yCum_znn[iB]>0.60) idxZoom.push_back(iB);
-	}
-	///
-	/// use the indices to build arrays
-	const UInt_t nZ = idxZoom.size();
-	Float_t yZoo_qcd[nZ];
-	Float_t yZoo_znn[nZ];
-	for(UInt_t iZ=0 ; iZ<nZ ; iZ++) {	
-	  yZoo_qcd[iZ] = yCum_qcd[idxZoom[iZ]];
-	  yZoo_znn[iZ] = yCum_znn[idxZoom[iZ]];
-	}
-	///
-	/// use the arrays to fill the graph and draw it
-	gRoc[iS][iV][iWd][1] = new TGraph(nZ, yZoo_znn, yZoo_qcd);
-	setStyle(gRoc[iS][iV][iWd][1], kOpenSquare, colors[iV], nZ,
-		 "ROC Curve : "+select[iS]+" "+var[iV]+" "+wdT[iWd],
-		 kTRUE);
-	if(gRoc[iS][iV][iWd][1]) gRoc[iS][iV][iWd][1]->Draw("AL");	
-	///
-	/// print the graph in a single pdf file
-	if(iS==0 && iV==0 && iWd==0) 
-	  cRoc.Print("plots/"+_tag+"/roczoom.pdf(",pdftitle);
-	else if(iS==nS-1 && iV==nV-1 && iWd==nWd-1)
-	  cRoc.Print("plots/"+_tag+"/roczoom.pdf)",pdftitle);
-	else
-	  cRoc.Print("plots/"+_tag+"/roczoom.pdf",pdftitle);
-	// end zoomed version
-	///////////////////////////////////////////////////////////////////////////////
-
-      } // end loop over nWd
-    }   // end loop over nV
-  }     // end loop over nS
-
-  TString metCut;
-  if(_tag.Contains("NoMetCut"))    metCut = "NoMetCut";
-  else if(_tag.Contains("Met200")) metCut = "Met200";
-  else if(_tag.Contains("Met350")) metCut = "Met350";
-  else if(_tag.Contains("MetFrom0to200"))   metCut = "MetFrom0to200";
-  else if(_tag.Contains("MetFrom200to250")) metCut = "MetFrom200to250";
-  else if(_tag.Contains("MetFrom250to350")) metCut = "MetFrom250to350";
-
-  // Print BoS plots
-  /*
-  for(UInt_t iS=0 ; iS<nS ; iS++) {
-    for(UInt_t iV=0 ; iV<nV ; iV++) {
-
-      if(hBoS[iS][iV]) {
-	hBoS[iS][iV]->SetTitle("Selection: "+select[iS]+" Variable: "+var[iV]);
-	setStyle(hBoS[iS][iV], kOpenSquare, colors[iV]);
-	hBoS[iS][iV]->Draw("P");
-      }
-
-      if(iV==0) 
-	cRoc.Print("plots/"+_tag+"/bos_"+select[iS]+".pdf(","Title:"+var[iV]);
-      else if(iV==nV-1)
-	cRoc.Print("plots/"+_tag+"/bos_"+select[iS]+".pdf)","Title:"+var[iV]);
-      else
-	cRoc.Print("plots/"+_tag+"/bos_"+select[iS]+".pdf","Title:"+var[iV]);
-    }
-  }
-  */
-
-  // Put several killers per plot
-  for(UInt_t iGZ=0 ; iGZ<nGZ ; iGZ++) {
-      
     for(UInt_t iS=0 ; iS<nS ; iS++) {
-
-      gRoc[iS][0][1][iGZ]->SetTitle("Selection: "+select[iS]);
-
-      if(gRoc[iS][0][1][iGZ]->GetN()>0) gRoc[iS][0][1][iGZ]->Draw("AL"); // alphat
-      else if(gRoc[iS][1][1][iGZ]->GetN()>0) gRoc[iS][1][1][iGZ]->Draw("AL");  // apcjetmetmax
-      else if(gRoc[iS][2][1][iGZ]->GetN()>0) gRoc[iS][2][1][iGZ]->Draw("AL");  // apcjetmetmin
-      else if(gRoc[iS][3][0][iGZ]->GetN()>0) gRoc[iS][3][0][iGZ]->Draw("AL");  // jetjetdphi
-      else if(gRoc[iS][4][1][iGZ]->GetN()>0) gRoc[iS][4][1][iGZ]->Draw("AL");  // jetmetdphimin
-
-      if(gRoc[iS][0][1][iGZ]->GetN()>0) gRoc[iS][0][1][iGZ]->Draw("L"); // alphat
-      if(gRoc[iS][1][1][iGZ]->GetN()>0) gRoc[iS][1][1][iGZ]->Draw("L");  // apcjetmetmax
-      if(gRoc[iS][2][1][iGZ]->GetN()>0) gRoc[iS][2][1][iGZ]->Draw("L");  // apcjetmetmin
-      if(gRoc[iS][3][0][iGZ]->GetN()>0) gRoc[iS][3][0][iGZ]->Draw("L");  // jetjetdphi
-      if(gRoc[iS][4][1][iGZ]->GetN()>0) gRoc[iS][4][1][iGZ]->Draw("L");  // jetmetdphimin
-
-      TLegend* leg = new TLegend(0.38,0.6,0.58,0.8,"","brNDC");
-      setStyle(leg);
     
-      for(UInt_t iV=0 ; iV<nV ; iV++)
-	leg->AddEntry(gRoc[iS][iV][1][iGZ], var[iV], "L");
+      cout << "-- selection: " << select[iS] << endl;
 
-      leg->Draw();
+      for(UInt_t iV=0 ; iV<nV ; iV++) {
 
-      TString mysel = select[iS]+"_"+metCut;
+	cout << "--- variable: " << var[iV] << endl;
 
-      if(iS==0) 
-	cRoc.Print("plots/"+_tag+"/allroc"+tzoom[iGZ]+".pdf(","Title:"+mysel);
-      else if(iS==nS-1)
-	cRoc.Print("plots/"+_tag+"/allroc"+tzoom[iGZ]+".pdf)","Title:"+mysel);
-      else
-	cRoc.Print("plots/"+_tag+"/allroc"+tzoom[iGZ]+".pdf","Title:"+mysel);
+	// Get input QCD and Znn distributions from the file
+	TString nameQCD = "h_"+var[iV]+"_qcd_"+select[iS]+"_"+scanCut[iC];
+	cout << "--- getting: " << nameQCD ;
+	TH1F* h_qcd = (TH1F*) file->Get(nameQCD);
+	cout << "... done!" << endl;
 
+	TString nameZnn = "h_"+var[iV]+"_znn_"+select[iS]+"_"+scanCut[iC];
+	cout << "--- getting: " << nameQCD ;
+	TH1F* h_znn = (TH1F*) file->Get(nameZnn);
+	cout << "... done!" << endl;
+
+	// B/S histogram
+	//hBoS[iS][iV] = (TH1F*)h_znn->Clone("hBoS_"+var[iV]+"_"+select[iS]);
+	//hBoS[iS][iV]->Reset();
+
+	// Check that the histograms were found
+	if(!h_qcd) {
+	  cout << "ERROR: missing histogram" << endl
+	       << nameQCD << endl;
+	}
+	if(!h_znn) {
+	  cout << "ERROR: missing histogram" << endl
+	       << nameZnn << endl;
+	}
+	if(!h_qcd || !h_znn) return -1;
+
+	// Print the integral of the histograms
+	cout << "--- going to print the fucking integral." << endl;
+	yieldQCD = h_qcd->Integral();
+	yieldZNN = h_znn->Integral();
+	if(unity && yieldQCD!=0) h_qcd->Scale(1/yieldQCD);
+	if(unity && yieldZNN!=0) h_znn->Scale(1/yieldZNN);
+	cout << "-- "+var[iV]+" "+select[iS]+" : Integrals : "
+	     << "qcd=" << yieldQCD << " "
+	     << "znn=" << yieldZNN << " " << endl;
+
+	// Get the histograms binning
+	//const UInt_t nB = nBins[iV];
+	const UInt_t nB = h_qcd->GetNbinsX();
+
+	// Build the graph from cumulative distributions
+	// for each backward/forward case
+	for(UInt_t iWd=0 ; iWd<nWd ; iWd++) {
+	  Float_t cum_qcd=0, cum_znn=0;
+	  Float_t yCum_qcd[nB];
+	  Float_t yCum_znn[nB];
+
+	  // Prepare full version of the graph
+	  /// Loop over the bins of the input distributions
+	  for(UInt_t iB=0 ; iB<nB ; iB++) {
+	    cum_qcd += h_qcd->GetBinContent(iB+1);
+	    cum_znn += h_znn->GetBinContent(iB+1);
+	    if(iWd==0) { // upper cut : x<cut
+	      yCum_qcd[iB] = cum_qcd;
+	      yCum_znn[iB] = cum_znn;
+	    }
+	    else if(iWd==1) { // lower cut : x>cut
+	      yCum_qcd[iB] = 1 - cum_qcd;
+	      yCum_znn[iB] = 1 - cum_znn;
+	    }
+
+	    // B/S profile
+	    //error  = 0;
+	    //theBos = calcBoS(yieldZNN, yieldQCD, yCum_znn[iB], yCum_qcd[iB], error);
+	    //hBoS[iS][iV]->SetBinContent(iB, theBos);
+	    //hBoS[iS][iV]->SetBinError(  iB, error);
+
+	    // Find bin index corresponding to requested QCD/ZNN efficiency (wpQCD/wpZNN)
+	    if(iB>0) {
+	      if(     iWd==0) { // upper cut x<cut
+		if(yCum_qcd[iB-1]<wpQCD && yCum_qcd[iB]>=wpQCD) wpQCD_bin = iB;
+		if(yCum_znn[iB-1]<wpZNN && yCum_znn[iB]>=wpZNN) wpZNN_bin = iB;
+	      }
+	      else if(iWd==1) { // lower cut x>cut
+		if(yCum_qcd[iB-1]>=wpQCD && yCum_qcd[iB]<wpQCD) wpQCD_bin = iB;
+		if(yCum_znn[iB-1]>=wpZNN && yCum_znn[iB]<wpZNN) wpZNN_bin = iB;
+	      }
+	    }
+	  }
+
+	  ///////////////////////////////////////////////////////////////////////////////
+	  // WORKING POINTS /////////////////////////////////////////////////////////////
+	  ///////////////////////////////////////////////////////////////////////////////
+	  // Find cut value using bin index for requested QCD/ZNN eff
+	  wpQCD_cut = h_qcd->GetXaxis()->GetBinCenter(wpQCD_bin);
+	  wpZNN_cut = h_znn->GetXaxis()->GetBinCenter(wpZNN_bin);
+	  //
+	  // Determine corresponding efficiencies
+	  // => precise value, for both QCD and ZNN, for each requested efficiency
+	  effQCD_wpQCD = (wpQCD_bin>=0 && wpQCD_bin<nB) ? yCum_qcd[wpQCD_bin] : -888888;
+	  effZNN_wpQCD = (wpQCD_bin>=0 && wpQCD_bin<nB) ? yCum_znn[wpQCD_bin] : -888888;
+	  effQCD_wpZNN = (wpZNN_bin>=0 && wpZNN_bin<nB) ? yCum_qcd[wpZNN_bin] : -888888;
+	  effZNN_wpZNN = (wpZNN_bin>=0 && wpZNN_bin<nB) ? yCum_znn[wpZNN_bin] : -888888;
+	  //
+	  // Write out values
+	  outlog << setw(10) << select[iS]
+		 << setw(14) << var[iV]
+		 << setw(12) << wdT[iWd]
+		 << setw(14) << yieldQCD
+		 << setw(14) << yieldZNN
+		 << setw(7 ) << wpQCD
+		 << setw(14) << wpQCD_cut
+		 << setw(14) << effQCD_wpQCD
+		 << setw(14) << effZNN_wpQCD
+		 << setw(14) << yieldQCD*effQCD_wpQCD
+		 << setw(14) << yieldZNN*effZNN_wpQCD
+		 << setw(7 ) << wpZNN
+		 << setw(14) << wpZNN_cut
+		 << setw(14) << effQCD_wpZNN
+		 << setw(14) << effZNN_wpZNN
+		 << setw(14) << yieldQCD*effQCD_wpZNN
+		 << setw(14) << yieldZNN*effZNN_wpZNN
+		 << endl;
+	  ///////////////////////////////////////////////////////////////////////////////
+
+	  ///////////////////////////////////////////////////////////////////////////////
+	  // GRAPHS /////////////////////////////////////////////////////////////////////
+	  ///////////////////////////////////////////////////////////////////////////////
+
+	  /// use the arrays to fill the graph and draw it
+	  gRoc[iS][iV][iWd][0] = new TGraph(nB, yCum_znn, yCum_qcd);
+	  setStyle(gRoc[iS][iV][iWd][0], kOpenSquare, colors[iV], nB,
+		   "ROC Curve : "+select[iS]+" "+var[iV]+" "+wdT[iWd], 
+		   kFALSE);
+	  gRoc[iS][iV][iWd][0]->SetTitle("Selection: "+select[iS]);
+	  if(gRoc[iS][iV][iWd][0]) gRoc[iS][iV][iWd][0]->Draw("AL");	
+	  ///
+	  /// print the graph in a single pdf file
+	  TString pdftitle = "Title:"+select[iS]+" "+var[iV]+" "+wd[iWd];
+	  if(iS==0 && iV==0 && iWd==0) 
+	    cRoc.Print("plots/"+_tag+"/rocfull_"+scanCut[iC]+".pdf(",pdftitle);
+	  else if(iS==nS-1 && iV==nV-1 && iWd==nWd-1)
+	    cRoc.Print("plots/"+_tag+"/rocfull_"+scanCut[iC]+".pdf)",pdftitle);
+	  else
+	    cRoc.Print("plots/"+_tag+"/rocfull_"+scanCut[iC]+".pdf",pdftitle);
+	  // end full version
+
+	  // Prepare zoomed version by extracting interesting region
+	  /// determine indices of points where eff(znn)>60%
+	  vector<UInt_t> idxZoom;
+	  idxZoom.clear();
+	  for(UInt_t iB=0 ; iB<nB ; iB++) {
+	    if(yCum_znn[iB]>0.60) idxZoom.push_back(iB);
+	  }
+	  ///
+	  /// use the indices to build arrays
+	  const UInt_t nZ = idxZoom.size();
+	  Float_t yZoo_qcd[nZ];
+	  Float_t yZoo_znn[nZ];
+	  for(UInt_t iZ=0 ; iZ<nZ ; iZ++) {	
+	    yZoo_qcd[iZ] = yCum_qcd[idxZoom[iZ]];
+	    yZoo_znn[iZ] = yCum_znn[idxZoom[iZ]];
+	  }
+	  ///
+	  /// use the arrays to fill the graph and draw it
+	  gRoc[iS][iV][iWd][1] = new TGraph(nZ, yZoo_znn, yZoo_qcd);
+	  setStyle(gRoc[iS][iV][iWd][1], kOpenSquare, colors[iV], nZ,
+		   "ROC Curve : "+select[iS]+" "+var[iV]+" "+wdT[iWd],
+		   kTRUE);
+	  gRoc[iS][iV][iWd][1]->SetTitle("Selection: "+select[iS]);
+	  if(gRoc[iS][iV][iWd][1]) gRoc[iS][iV][iWd][1]->Draw("AL");	
+	  ///
+	  /// print the graph in a single pdf file
+	  if(iS==0 && iV==0 && iWd==0) 
+	    cRoc.Print("plots/"+_tag+"/roczoom_"+scanCut[iC]+".pdf(",pdftitle);
+	  else if(iS==nS-1 && iV==nV-1 && iWd==nWd-1)
+	    cRoc.Print("plots/"+_tag+"/roczoom_"+scanCut[iC]+".pdf)",pdftitle);
+	  else
+	    cRoc.Print("plots/"+_tag+"/roczoom_"+scanCut[iC]+".pdf",pdftitle);
+	  // end zoomed version
+	  ///////////////////////////////////////////////////////////////////////////////
+
+	} // end loop over nWd
+      }   // end loop over nV
+    }     // end loop over nS
+
+    TString metCut = scanCut[iC];
+    /*
+      if(_tag.Contains("NoMetCut"))    metCut = "NoMetCut";
+      else if(_tag.Contains("Met200")) metCut = "Met200";
+      else if(_tag.Contains("Met350")) metCut = "Met350";
+      else if(_tag.Contains("MetFrom0to200"))   metCut = "MetFrom0to200";
+      else if(_tag.Contains("MetFrom200to250")) metCut = "MetFrom200to250";
+      else if(_tag.Contains("MetFrom250to350")) metCut = "MetFrom250to350";
+    */
+
+    // Put several killers per plot
+    for(UInt_t iGZ=0 ; iGZ<nGZ ; iGZ++) {
+      
+      for(UInt_t iS=0 ; iS<nS ; iS++) {
+	
+	/*
+	for(UInt_t iV=0 ; iV<nV ; iV++) {
+	  gRoc[iS][iV][1][iGZ]->SetTitle("Selection: "+select[iS]);
+	}
+	*/
+
+	if(gRoc[iS][0][1][iGZ]->GetN()>0) gRoc[iS][0][1][iGZ]->Draw("AL"); // alphat
+	else if(gRoc[iS][1][1][iGZ]->GetN()>0) gRoc[iS][1][1][iGZ]->Draw("AL");  // apcjetmetmax
+	else if(gRoc[iS][2][1][iGZ]->GetN()>0) gRoc[iS][2][1][iGZ]->Draw("AL");  // apcjetmetmin
+	else if(gRoc[iS][3][0][iGZ]->GetN()>0) gRoc[iS][3][0][iGZ]->Draw("AL");  // jetjetdphi
+	else if(gRoc[iS][4][1][iGZ]->GetN()>0) gRoc[iS][4][1][iGZ]->Draw("AL");  // jetmetdphimin
+
+	if(gRoc[iS][0][1][iGZ]->GetN()>0) gRoc[iS][0][1][iGZ]->Draw("L"); // alphat
+	if(gRoc[iS][1][1][iGZ]->GetN()>0) gRoc[iS][1][1][iGZ]->Draw("L");  // apcjetmetmax
+	if(gRoc[iS][2][1][iGZ]->GetN()>0) gRoc[iS][2][1][iGZ]->Draw("L");  // apcjetmetmin
+	if(gRoc[iS][3][0][iGZ]->GetN()>0) gRoc[iS][3][0][iGZ]->Draw("L");  // jetjetdphi
+	if(gRoc[iS][4][1][iGZ]->GetN()>0) gRoc[iS][4][1][iGZ]->Draw("L");  // jetmetdphimin
+
+	TLegend* leg;
+	if(iGZ==0)      leg = new TLegend(0.13,0.69,0.33,0.89,"","brNDC");
+	else if(iGZ==1) leg = new TLegend(0.69,0.14,0.89,0.34,"","brNDC");
+	else            leg = new TLegend(0.38,0.6,0.58,0.8,"","brNDC");
+	setStyle(leg);
+    
+	for(UInt_t iV=0 ; iV<nV ; iV++)
+	  leg->AddEntry(gRoc[iS][iV][1][iGZ], var[iV], "L");
+
+	leg->Draw();
+
+	TString mysel = select[iS]+"_"+metCut;
+
+	if(iS==0) 
+	  cRoc.Print("plots/"+_tag+"/allroc_"+scanCut[iC]+tzoom[iGZ]+".pdf(","Title:"+mysel);
+	else if(iS==nS-1)
+	  cRoc.Print("plots/"+_tag+"/allroc_"+scanCut[iC]+tzoom[iGZ]+".pdf)","Title:"+mysel);
+	else
+	  cRoc.Print("plots/"+_tag+"/allroc_"+scanCut[iC]+tzoom[iGZ]+".pdf","Title:"+mysel);
+
+      }
     }
   }
   
