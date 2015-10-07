0d364e9c33dcaf729eef52fc17bbbd42bca01c31
ndaci <daci.nadir@gmail.com>
2015-10-07 11:48:16 +0200

    Revert back to old methods: uncorPt()->uncorrectedPt(), uncorPhi()->uncorrectedPhi(), corPt()->et(), corPhi()->phi().

--- MonoXAnalysis/AnalysisStep/plugins/MonoJetTreeMaker.cc
+++ MonoXAnalysis/AnalysisStep/plugins/MonoJetTreeMaker.cc
@@ -635,14 +635,14 @@ void MonoJetTreeMaker::analyze(const edm::Event& iEvent, const edm::EventSetup& 
     pmetphi = 0.;
     ometphi = 0.;
 
-    t1pfmet        = t1pfmetH->front().corPt();
-    t1pfmetphi     = t1pfmetH->front().corPhi();
+    t1pfmet        = t1pfmetH->front().et();
+    t1pfmetphi     = t1pfmetH->front().phi();
 
     calomet        = t1pfmetH->front().caloMETPt();
     calometphi     = t1pfmetH->front().caloMETPhi();
 
-    pfmet          = t1pfmetH->front().uncorPt();
-    pfmetphi       = t1pfmetH->front().uncorPhi();
+    pfmet          = t1pfmetH->front().uncorrectedPt();
+    pfmetphi       = t1pfmetH->front().uncorrectedPhi();
 
     if (partmetH.isValid() && partmetH->size() == 9) {
     hmet           = (*partmetH)[1].et();
