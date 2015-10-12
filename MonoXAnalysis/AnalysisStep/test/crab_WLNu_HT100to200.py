c428ba190cb540d4dca92b49a27bf3ec3a1f50ad
ndaci <daci.nadir@gmail.com>
2015-10-12 08:35:15 +0200

    Add crab config files to process ZNN and WLN HT-binned samples.

---
+++ MonoXAnalysis/AnalysisStep/test/crab_WLNu_HT100to200.py
@@ -0,0 +1,51 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+name = 'EffAN_7413_Spring15'
+proc = 'WLNu_HT100to200_V7'
+dataset = '/WJetsToLNu_HT-100To200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
+
+# GENERAL
+config.section_("General")
+config.General.requestName = name+"_"+proc
+config.General.workArea    = '/user/ndaci/CRABBY/StudyHLT/Monojet/'+name+'/'+proc+'/'
+config.General.transferLogs    = True
+#config.General.transferOutput = True
+
+# JOB TYPE
+config.section_("JobType")
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'nadirMC.py'
+config.JobType.pyCfgParams = ['theXSec=1343000']
+##config.JobType.inputFiles = '../../data/dttf_config.db'
+#config.JobType.inputFiles = ['Summer15_50nsV4_MC.db']
+config.JobType.allowUndistributedCMSSW = True
+
+# INPUT DATA
+config.section_("Data")
+config.Data.inputDataset = dataset
+config.Data.inputDBS = 'https://cmsweb.cern.ch/dbs/prod/global/DBSReader/'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+#config.Data.totalUnits  = 1976
+config.Data.publication = False
+config.Data.publishDBS  = 'https://cmsweb.cern.ch/dbs/prod/phys03/DBSWriter/'
+config.Data.publishDataName = name+"_"+proc
+config.Data.ignoreLocality = False # allows to process inputs on CE != site hosting inputs
+#config.Data.lumiMask = 
+#config.Data.runRange = 
+
+#A custom string to insert in the output file name inside the CRAB-created directory path to allow organizing groups of tasks.
+#config.Data.prefix =  
+
+# USER
+config.section_("User")
+#config.User.email = 'nadir.daci@cern.ch'
+#config.User.voRole = 
+config.User.voGroup = 'becms'
+
+# GRID
+config.section_("Site")
+config.Site.storageSite = 'T2_BE_IIHE'
+#config.Site.whitelist = ['T2_BE_IIHE']
+#config.Site.blacklist = ['T1_US_FNAL']
