4cf866f5977f1d839206de0e05268d1bb5d93f2e
ndaci <daci.nadir@gmail.com>
2015-10-21 15:48:58 +0200

    Add extra TString layer to the _mapHistos object, to save histograms per selection. Reminder: the 'plot' method is called once per selection. Now, the histograms are kept for all selections, instead of being kept only for the last selection in the calls to 'plot'. Enjoy.

--- MonoXAnalysis/AnalysisStep/macros/XMetAnalysis.h
+++ MonoXAnalysis/AnalysisStep/macros/XMetAnalysis.h
@@ -9,9 +9,13 @@ using namespace std;
 //typedef map< TString , pair< TChain* , pair< vector<TString> , Int_t > > > MAP_PROCESS;
 typedef map< TString , XMetProcess > MAP_PROCESS;
 
-typedef map<TString,TH1F*> M_VAR_H;
-typedef map<TString, map<TString,TH1F*> > M_CUT_VAR_H;
-typedef map<TString, map<TString, map<TString,TH1F*> > > M_PROCESS_CUT_VAR_H;
+typedef map<TString, TH1F*>                M_VAR_H;
+typedef map<TString, M_VAR_H >             M_CUT_VAR_H;
+typedef map<TString, M_CUT_VAR_H >         M_PROC_CUT_VAR_H;
+typedef map<TString, M_PROC_CUT_VAR_H >    M_SEL_PROC_CUT_VAR_H;
+
+// variables and axes
+typedef map<TString, TString> M_VAR_AXIS;
 
 class XMetAnalysis {
 
@@ -25,15 +29,25 @@ class XMetAnalysis {
   Int_t DefineChainsAN15();
 
   // Analyses
-  Int_t Analysis();
-  Int_t AnalysisRun1();
-  Int_t AnalysisAN15();
+  Int_t Analysis(Bool_t bProdHistos);
+  Int_t AnalysisRun1(Bool_t bProdHistos);
+  Int_t AnalysisAN15(Bool_t bProdHistos);
   Int_t StudyQCDKiller(TString signal);
   
-  Int_t plot(TString select, 			 
-	     const UInt_t nCut, TString* scanCut, Bool_t* scanReset,
-	     const UInt_t nV, TString* var, 
-	     UInt_t* nBins, Float_t* xFirst, Float_t* xLast, 
+  Int_t GetHistos(const UInt_t nS  , TString* select, 
+		  const UInt_t nCut, TString* scanCut,
+		  const UInt_t nV  , TString* var, 
+		  vector<TString> locProcesses);
+
+  Int_t DrawStackPlots(const UInt_t nS  , TString* select, 
+		       const UInt_t nCut, TString* scanCut,
+		       const UInt_t nV  , TString* var,
+		       vector<TString> locProcesses);
+
+  Int_t plot(TString select, 
+	     const UInt_t nCut, TString *scanCut, Bool_t *scanReset,
+	     const UInt_t nV,    TString *var, 
+	     const UInt_t *xbins, Float_t **v_bins,
 	     vector<TString> locProcesses);
 
   TCut  defineCut(TString select, TString region);
@@ -43,8 +57,18 @@ class XMetAnalysis {
   TFile*   _outfile;
   ofstream* _outlog;
   //
+  M_SEL_PROC_CUT_VAR_H _mapHistos;
+  M_SEL_PROC_CUT_VAR_H::iterator _itSelProcCutVarH;
+  M_PROC_CUT_VAR_H::iterator     _itProcCutVarH;
+  M_CUT_VAR_H::iterator          _itCutVarH;
+  M_VAR_H::iterator              _itVarH;
+  //
   MAP_PROCESS _mapProcess;
   MAP_PROCESS::iterator _itProcess;
+  //
+  // variables and axes
+  M_VAR_AXIS _Axis;
+  M_VAR_AXIS _Title;
   //
   TString  _tag,  _pathMC, _pathData, _dirOut;
   Bool_t   _isAN15, _isRun1;
