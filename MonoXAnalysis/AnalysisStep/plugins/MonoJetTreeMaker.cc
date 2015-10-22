113bf847579564deb8e3a914886609ed3311bd50
ndaci <daci.nadir@gmail.com>
2015-10-22 12:06:46 +0200

    Make trigger objects optional.

--- MonoXAnalysis/AnalysisStep/plugins/MonoJetTreeMaker.cc
+++ MonoXAnalysis/AnalysisStep/plugins/MonoJetTreeMaker.cc
@@ -149,6 +149,7 @@ class MonoJetTreeMaker : public edm::EDAnalyzer {
         double   zmass, zpt, zeta, zphi, wmt, emumass, emupt, emueta, emuphi, zeemass, zeept, zeeeta, zeephi, wemt;
         double   xsec, wgt, kfact, puwgt;
         int32_t  _verbose;
+        bool     _useTrigObj;
 
         // Trigger objects
         TString _trig_pass;
@@ -162,7 +163,7 @@ class MonoJetTreeMaker : public edm::EDAnalyzer {
         //std::vector< std::vector<int> > _trig_obj_ids;
 
         // Jet informations
-        const static UInt_t _nMaxJets=10;
+        const static UInt_t _nMaxJets=20;
         uint32_t _jet_n;
         double _jet_pt[_nMaxJets], _jet_eta[_nMaxJets], _jet_phi[_nMaxJets], 
 	  _jet_CHfrac[_nMaxJets], _jet_NHfrac[_nMaxJets], _jet_EMfrac[_nMaxJets], _jet_CEMfrac[_nMaxJets],
@@ -242,7 +243,8 @@ MonoJetTreeMaker::MonoJetTreeMaker(const edm::ParameterSet& iConfig):
     uselheweights(iConfig.existsAs<bool>("uselheweights") ? iConfig.getParameter<bool>("uselheweights") : false),
     xsec(iConfig.getParameter<double>("xsec") * 1000.0),
     kfact((iConfig.existsAs<double>("kfactor") ? iConfig.getParameter<double>("kfactor") : 1.0)),
-    _verbose((iConfig.existsAs<int32_t>("verbose") ? iConfig.getParameter<int32_t>("verbose") : 0))
+    _verbose((iConfig.existsAs<int32_t>("verbose") ? iConfig.getParameter<int32_t>("verbose") : 0)),
+    _useTrigObj((iConfig.existsAs<bool>("useTrigObj") ? iConfig.getParameter<bool>("useTrigObj") : 0))
 {
 }
 
@@ -529,100 +531,104 @@ void MonoJetTreeMaker::analyze(const edm::Event& iEvent, const edm::EventSetup& 
     ///bool isNone, isL3, isLF, isBoth;
     int iObj=0;
 
-    if(_verbose>3) cout << "Loop over trigger objects: " << H_trg_obj->size() << " elements." << endl;
+    if(_useTrigObj) {
 
-    // Loop over trigger objects    
-    for (pat::TriggerObjectStandAlone obj : *H_trg_obj) { // note: not "const &" since we want to call unpackPathNames
+      if(_verbose>3) cout << "Loop over trigger objects: " << H_trg_obj->size() << " elements." << endl;
       
-      if(_verbose>3) cout << "-- iteration #" << iObj << endl;
-      iObj++ ;
-
-      obj.unpackPathNames(triggerNames);
-      
-      // pt,eta,phi
-      if(_verbose>1) cout << "\tTrigger object:  pt " << obj.pt() << ", eta " << obj.eta() << ", phi " << obj.phi() << endl;
-      //
-      _trig_obj_pt.push_back( obj.pt());
-      _trig_obj_eta.push_back(obj.eta());
-      _trig_obj_phi.push_back(obj.phi());
-
-      // Collection
-      trgColl = obj.collection();
-      if(_verbose>1) cout << "\t   Collection: " << trgColl << endl;
-      //
-      _trig_obj_col.push_back(trgColl);
-      
-      // Trigger Filter Ids
-      if(_verbose>1) cout << "\t   Type IDs:   ";
-      trgIds = obj.filterIds();
-      //
-      //_trig_obj_ids.push_back(trgIds);
-      //
-      for (unsigned h = 0; h < trgIds.size(); ++h) {
-	if(_verbose>1) cout << " " << trgIds[h] ;
-      }
-      if(_verbose>1) cout << endl;
-      
-      // Trigger filters
-      if(_verbose>1) cout << "\t   Filters:    ";
-      trgFilt = obj.filterLabels();
-      for (unsigned h = 0; h < trgFilt.size(); ++h) {
-	if(_verbose>1) cout << " " << trgFilt[h];
-	trgFiltStr += trgFilt[h]+"_%_" ;
-      }
-      if(_verbose>1) cout << endl;
-      _trig_obj_lab.push_back(trgFiltStr);
-
-      // Trigger paths
-      /*
-      trgPathsFF = obj.pathNames(false,false);
-      trgPathsFT = obj.pathNames(false,true);
-      trgPathsTF = obj.pathNames(true,false);
-      trgPathsTT = obj.pathNames(true,true);
-      //
-      _trig_obj_path_FT.push_back(concatenate(trgPathsFT));
-      _trig_obj_path_TF.push_back(concatenate(trgPathsTF));
-      _trig_obj_path_TT.push_back(concatenate(trgPathsTT));
-      //
-      if(_verbose>1) cout << "\t   Paths (" << trgPathsFF.size()<<"/"<<trgPathsTT.size()<<"):    ";
-      //
-
-      // Loop over all associated paths
-
-      for (unsigned h = 0, n = trgPathsFF.size(); h < n; ++h) {
+      // Loop over trigger objects    
+      for (pat::TriggerObjectStandAlone obj : *H_trg_obj) { // note: not "const &" since we want to call unpackPathNames
 	
-	trgPathsFFStr += trgPathsFF[h]+"_%_";      
+	if(_verbose>3) cout << "-- iteration #" << iObj << endl;
+	iObj++ ;
 	
-	isNone = obj.hasPathName( trgPathsFF[h], false, false ); 
-	isL3   = obj.hasPathName( trgPathsFF[h], false, true ); 
-	isLF   = obj.hasPathName( trgPathsFF[h], true, false ); 
-	isBoth = obj.hasPathName( trgPathsFF[h], true, true ); 
+	obj.unpackPathNames(triggerNames);
 	
-	if(_verbose>1) {
+	// pt,eta,phi
+	if(_verbose>1) cout << "\tTrigger object:  pt " << obj.pt() << ", eta " << obj.eta() << ", phi " << obj.phi() << endl;
+	//
+	_trig_obj_pt.push_back( obj.pt());
+	_trig_obj_eta.push_back(obj.eta());
+	_trig_obj_phi.push_back(obj.phi());
+	
+	// Collection
+	trgColl = obj.collection();
+	if(_verbose>1) cout << "\t   Collection: " << trgColl << endl;
+	//
+	_trig_obj_col.push_back(trgColl);
+	
+	// Trigger Filter Ids
+	if(_verbose>1) cout << "\t   Type IDs:   ";
+	trgIds = obj.filterIds();
+	//
+	//_trig_obj_ids.push_back(trgIds);
+	//
+	for (unsigned h = 0; h < trgIds.size(); ++h) {
+	  if(_verbose>1) cout << " " << trgIds[h] ;
+	}
+	if(_verbose>1) cout << endl;
+      
+	// Trigger filters
+	if(_verbose>1) cout << "\t   Filters:    ";
+	trgFilt = obj.filterLabels();
+	for (unsigned h = 0; h < trgFilt.size(); ++h) {
+	  if(_verbose>1) cout << " " << trgFilt[h];
+	  trgFiltStr += trgFilt[h]+"_%_" ;
+	}
+	if(_verbose>1) cout << endl;
+	_trig_obj_lab.push_back(trgFiltStr);
+
+	// Trigger paths
+	/*
+	  trgPathsFF = obj.pathNames(false,false);
+	  trgPathsFT = obj.pathNames(false,true);
+	  trgPathsTF = obj.pathNames(true,false);
+	  trgPathsTT = obj.pathNames(true,true);
+	  //
+	  _trig_obj_path_FT.push_back(concatenate(trgPathsFT));
+	  _trig_obj_path_TF.push_back(concatenate(trgPathsTF));
+	  _trig_obj_path_TT.push_back(concatenate(trgPathsTT));
+	  //
+	  if(_verbose>1) cout << "\t   Paths (" << trgPathsFF.size()<<"/"<<trgPathsTT.size()<<"):    ";
+	  //
+
+	  // Loop over all associated paths
+
+	  for (unsigned h = 0, n = trgPathsFF.size(); h < n; ++h) {
+	
+	  trgPathsFFStr += trgPathsFF[h]+"_%_";      
+	
+	  isNone = obj.hasPathName( trgPathsFF[h], false, false ); 
+	  isL3   = obj.hasPathName( trgPathsFF[h], false, true ); 
+	  isLF   = obj.hasPathName( trgPathsFF[h], true, false ); 
+	  isBoth = obj.hasPathName( trgPathsFF[h], true, true ); 
+	
+	  if(_verbose>1) {
 	  cout << "   " << trgPathsFF[h];
 	  if (isBoth)  cout << "(L,3)";
 	  if (isL3 && !isBoth)  cout << "(*,3)";
 	  if (isLF && !isBoth)  cout << "(L,*)";
 	  if (isNone && !isBoth && !isL3 && !isLF)  cout << "(*,*)";
-	}
-      }
-      if(_verbose>1) cout << endl;
-      //
-      _trig_obj_path_FF.push_back(trgPathsFFStr);
-      */
+	  }
+	  }
+	  if(_verbose>1) cout << endl;
+	  //
+	  _trig_obj_path_FF.push_back(trgPathsFFStr);
+	*/
       
-      // Clear vectors
-      trgIds.clear();
-      trgFilt.clear(); 
-      trgFiltStr="";
-      trgColl="";
-      trgPathsFFStr="";
+	// Clear vectors
+	trgIds.clear();
+	trgFilt.clear(); 
+	trgFiltStr="";
+	trgColl="";
+	trgPathsFFStr="";
       
-      // Increment trigger object index
-      _trig_obj_n++ ;
-    }  
-    if(_verbose>3) cout << "- End trigger objects" << endl;
+	// Increment trigger object index
+	_trig_obj_n++ ;
+      }  
+      if(_verbose>3) cout << "- End trigger objects" << endl;
+    }
     //////////// END TRIGGER OBJECTS //////////////
+
 
     // Pileup info -- Will need to the updated to the Run-II specifications
     nvtx   = verticesH->size();
@@ -1381,7 +1387,10 @@ void MonoJetTreeMaker::beginJob() {
 
     edm::Service<TFileService> fs;
     tree = fs->make<TTree>("tree"       , "tree");
+
     int buffersize = 1000; // trig_obj vectors
+    if(_verbose>5) std::cout << "buffersize=" << buffersize << std::endl;
+
     // Run, Lumi, Event info
     tree->Branch("event"                , &event                , "event/i");
     tree->Branch("run"                  , &run                  , "run/i");
@@ -1414,19 +1423,21 @@ void MonoJetTreeMaker::beginJob() {
 
     // Trigger objects
     //tree->Branch("trig_pass",&_trig_pass);
-    tree->Branch("trig_n",&_trig_n,"trig_n/I");  
-    //
-    tree->Branch("trig_obj_n",&_trig_obj_n,"trig_obj_n/I");
-    tree->Branch("trig_obj_pt","std::vector<double>",&_trig_obj_pt,buffersize);
-    tree->Branch("trig_obj_eta","std::vector<double>",&_trig_obj_eta,buffersize);
-    tree->Branch("trig_obj_phi","std::vector<double>",&_trig_obj_phi,buffersize);
-    tree->Branch("trig_obj_col","std::vector<std::string>",&_trig_obj_col,buffersize);
-    tree->Branch("trig_obj_lab", "std::vector<std::string>",&_trig_obj_lab, buffersize);
-    //tree->Branch("trig_obj_ids","std::vector<std::vector<std::int>>",&_trig_obj_ids,buffersize);
-    //tree->Branch("trig_obj_path_FF","std::vector<std::string>",&_trig_obj_path_FF,buffersize);
-    //tree->Branch("trig_obj_path_FT","std::vector<std::string>",&_trig_obj_path_FT,buffersize);
-    //tree->Branch("trig_obj_path_TF","std::vector<std::string>",&_trig_obj_path_TF,buffersize);
-    //tree->Branch("trig_obj_path_TT","std::vector<std::string>",&_trig_obj_path_TT,buffersize);
+    if(_useTrigObj) {
+      tree->Branch("trig_n",&_trig_n,"trig_n/I");  
+      //
+      tree->Branch("trig_obj_n",&_trig_obj_n,"trig_obj_n/I");
+      tree->Branch("trig_obj_pt","std::vector<double>",&_trig_obj_pt,buffersize);
+      tree->Branch("trig_obj_eta","std::vector<double>",&_trig_obj_eta,buffersize);
+      tree->Branch("trig_obj_phi","std::vector<double>",&_trig_obj_phi,buffersize);
+      tree->Branch("trig_obj_col","std::vector<std::string>",&_trig_obj_col,buffersize);
+      tree->Branch("trig_obj_lab", "std::vector<std::string>",&_trig_obj_lab, buffersize);
+      //tree->Branch("trig_obj_ids","std::vector<std::vector<std::int>>",&_trig_obj_ids,buffersize);
+      //tree->Branch("trig_obj_path_FF","std::vector<std::string>",&_trig_obj_path_FF,buffersize);
+      //tree->Branch("trig_obj_path_FT","std::vector<std::string>",&_trig_obj_path_FT,buffersize);
+      //tree->Branch("trig_obj_path_TF","std::vector<std::string>",&_trig_obj_path_TF,buffersize);
+      //tree->Branch("trig_obj_path_TT","std::vector<std::string>",&_trig_obj_path_TT,buffersize);
+    }
 
     // Jet vectors
     tree->Branch("jet_n" ,&_jet_n );
